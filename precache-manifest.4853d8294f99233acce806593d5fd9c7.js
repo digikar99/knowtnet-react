dc76c4be37769eec78dc69e859e4d9bc1c4726d5
digikar99 <shubhamkarayare@gmail.com>
2019-12-18 18:29:55 +0530

    Updates

---
+++ precache-manifest.4853d8294f99233acce806593d5fd9c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5df410421bca88cdb17a4b8a2eaca19",
+    "url": "/knowtnet-react/index.html"
+  },
+  {
+    "revision": "aad8468a17fdc8224f9b",
+    "url": "/knowtnet-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "96e419e3e535f049a135",
+    "url": "/knowtnet-react/static/js/2.c4fd95b8.chunk.js"
+  },
+  {
+    "revision": "40fe45981378b6a729ae73536660914c",
+    "url": "/knowtnet-react/static/js/2.c4fd95b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aad8468a17fdc8224f9b",
+    "url": "/knowtnet-react/static/js/main.693502d0.chunk.js"
+  },
+  {
+    "revision": "4e93b0a7551b8ee2cbbb",
+    "url": "/knowtnet-react/static/js/runtime-main.b8a91a84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/knowtnet-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
